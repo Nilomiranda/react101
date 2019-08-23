f42c58ec2c8f7ad89463e198afc2c45c56565bf8
Nilomiranda <nilomiranda3@gmail.com>
2019-08-22 21:21:18 -0300

    Users can now view repository details and issues

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import api from '../../services/api';
+
+import { Loading, Owner, IssuesList } from './styles';
+import Container from '../../components/Container';
 
 export default class Repository extends Component {
   static propTypes = {
@@ -40,6 +44,37 @@ export default class Repository extends Component {
   }
 
   render() {
-    return <h1>Repository</h1>;
+    const { repository, issues, loading } = this.state;
+
+    if (loading) {
+      return <Loading>Carregando</Loading>;
+    }
+    return (
+      <Container>
+        <Owner>
+          <Link to="/">Return to repositories</Link>
+          <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+          <h1>{repository.name}</h1>
+          <p>{repository.description}</p>
+        </Owner>
+
+        <IssuesList>
+          {issues.map(issue => (
+            <li key={String(issue.id)}>
+              <img src={issue.user.avatar_url} alt={issue.user.login} />
+              <div>
+                <strong>
+                  <a href={issue.html_url}>{issue.title}</a>
+                  {issue.labels.map(label => (
+                    <span key={String(label.id)}>{label.name}</span>
+                  ))}
+                </strong>
+                <p>{issue.user.login}</p>
+              </div>
+            </li>
+          ))}
+        </IssuesList>
+      </Container>
+    );
   }
 }
