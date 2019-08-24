b67445e2147c2275dd9438c1b663b3f385603be6
Nilomiranda <nilomiranda3@gmail.com>
2019-08-24 17:26:23 -0300

    Updates

---
+++ precache-manifest.808f5e092b76ddc60397ca6b3d604c9b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f709b6c5be171b3c9363e471fd454b6",
+    "url": "/react101/index.html"
+  },
+  {
+    "revision": "5a87666e40800e8adb93",
+    "url": "/react101/static/js/2.6425258f.chunk.js"
+  },
+  {
+    "revision": "730aa5194e79b68e4006",
+    "url": "/react101/static/js/main.84838391.chunk.js"
+  },
+  {
+    "revision": "0f53af4fc9d8743d9da1",
+    "url": "/react101/static/js/runtime~main.6ee55df5.js"
+  }
+]);
